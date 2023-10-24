d825ae3cf4cb0acba1c89563b6c7b68c5a660548
0xDysis <99392905+0xDysis@users.noreply.github.com>
2023-10-25 01:03:34 +0200

    added functionality

--- script.js
+++ script.js
@@ -18,12 +18,30 @@ document.getElementById('removeOverlay').addEventListener('click', function() {
 
 document.getElementById('sizeSlider').addEventListener('input', function(e) {
     document.getElementById('overlayImage').style.width = e.target.value + '%';
+    document.getElementById('sizeInput').value = e.target.value;
+});
+
+document.getElementById('sizeInput').addEventListener('input', function(e) {
+    document.getElementById('overlayImage').style.width = e.target.value + '%';
+    document.getElementById('sizeSlider').value = e.target.value;
 });
 
 document.getElementById('xSlider').addEventListener('input', function(e) {
     document.getElementById('overlayImage').style.left = e.target.value + '%';
+    document.getElementById('xInput').value = e.target.value;
+});
+
+document.getElementById('xInput').addEventListener('input', function(e) {
+    document.getElementById('overlayImage').style.left = e.target.value + '%';
+    document.getElementById('xSlider').value = e.target.value;
 });
 
 document.getElementById('ySlider').addEventListener('input', function(e) {
     document.getElementById('overlayImage').style.top = e.target.value + '%';
+    document.getElementById('yInput').value = e.target.value;
+});
+
+document.getElementById('yInput').addEventListener('input', function(e) {
+    document.getElementById('overlayImage').style.top = e.target.value + '%';
+    document.getElementById('ySlider').value = e.target.value;
 });
