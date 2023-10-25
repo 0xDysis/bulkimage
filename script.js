1dedc8e002b586daa03b1bd5bd2a3e4191df1b75
0xDysis <99392905+0xDysis@users.noreply.github.com>
2023-10-25 02:21:32 +0200

    added bulk download

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+var overlayImages = [];
+
 document.getElementById('baseLayer').addEventListener('change', function(e) {
     document.getElementById('baseLayerImage').src = URL.createObjectURL(e.target.files[0]);
 });
 
-document.getElementById('overlay').addEventListener('change', function(e) {
-    document.getElementById('overlayImage').src = URL.createObjectURL(e.target.files[0]);
+document.getElementById('overlays').addEventListener('change', function(e) {
+    overlayImages = Array.from(e.target.files).map(function(file) {
+        return URL.createObjectURL(file);
+    });
+    document.getElementById('overlayImage').src = overlayImages[0];
 });
 
 document.getElementById('removeBaseLayer').addEventListener('click', function() {
@@ -11,9 +16,10 @@ document.getElementById('removeBaseLayer').addEventListener('click', function() 
     document.getElementById('baseLayer').value = '';
 });
 
-document.getElementById('removeOverlay').addEventListener('click', function() {
+document.getElementById('removeOverlays').addEventListener('click', function() {
     document.getElementById('overlayImage').src = '';
-    document.getElementById('overlay').value = '';
+    document.getElementById('overlays').value = '';
+    overlayImages = [];
 });
 
 document.getElementById('sizeSlider').addEventListener('input', function(e) {
@@ -50,36 +56,36 @@ document.getElementById('filterSelector').addEventListener('change', function(e)
     document.getElementById('overlayImage').style.filter = e.target.value;
 });
 
-document.getElementById('downloadImage').addEventListener('click', function() {
+document.getElementById('downloadImages').addEventListener('click', function() {
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
     var baseLayerImage = document.getElementById('baseLayerImage');
     var overlayImage = document.getElementById('overlayImage');
     canvas.width = baseLayerImage.width;
     canvas.height = baseLayerImage.height;
-    context.drawImage(baseLayerImage, 0, 0);
     var overlayWidth = overlayImage.offsetWidth / baseLayerImage.offsetWidth * baseLayerImage.width;
     var overlayHeight = overlayImage.offsetHeight / baseLayerImage.offsetHeight * baseLayerImage.height;
     var overlayX = parseFloat(overlayImage.style.left) / 100 * baseLayerImage.width;
     var overlayY = parseFloat(overlayImage.style.top) / 100 * baseLayerImage.height;
+    var filter = overlayImage.style.filter;
 
-    // Save the current context state
-    context.save();
-
-    // Apply the filter to the context
-    context.filter = overlayImage.style.filter;
-
-    // Draw the overlay image with the filter applied
-    context.drawImage(overlayImage, overlayX, overlayY, overlayWidth, overlayHeight);
-
-    // Restore the context state to remove the filter
-    context.restore();
-
-    var dataUrl = canvas.toDataURL('image/png');
-    var a = document.createElement('a');
-    a.href = dataUrl;
-    a.download = 'combined.png';
-    a.click();
+    overlayImages.forEach(function(src, index) {
+        var img = new Image();
+        img.onload = function() {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(baseLayerImage, 0, 0);
+            context.save();
+            context.filter = filter;
+            context.drawImage(img, overlayX, overlayY, overlayWidth, overlayHeight);
+            context.restore();
+            var dataUrl = canvas.toDataURL('image/png');
+            var a = document.createElement('a');
+            a.href = dataUrl;
+            a.download = 'combined' + index + '.png';
+            a.click();
+        };
+        img.src = src;
+    });
 });
 
 
